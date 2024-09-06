008fac405d71439fcbcaa10247b274290317786b
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-09-06 16:47:54 -0300

    Initial commit

---
+++ Assets/Script/SwarmManager.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwarmTesting
+{
+    /// <summary>
+    /// Coordinates the behavior of the swarm.
+    /// </summary>
+    //[CreateAssetMenu(fileName = "SwarmManager", menuName = "ScriptableObjects/SwarmManager", order = 1)]
+    internal class SwarmManager : MonoBehaviour
+    {
+        [SerializeField] GameObject prefab;
+        // The number of foes to be spawned
+        [SerializeField] int spawnCount = 50;
+
+        /// <summary>
+        /// The distance SwarmObjects should attempt to keep from other objects.
+        /// </summary>
+        [SerializeField] public float AvoidanceDist = 5;
+
+        /// <summary>
+        /// Used to calculate how much an object should avoid things that are close to it, or go towards the center if far away.
+        /// </summary>
+        [SerializeField] public float WeightFactor = 2;
+
+        /// <summary>
+        /// The distance from the average at which recall weight is minimized.
+        /// </summary>
+        [SerializeField] public float MinRecallDistance = 25;
+
+        /// <summary>
+        /// The distance from the average at which recall weight is maximized.
+        /// </summary>
+        [SerializeField] public float MaxRecallDistance = 50;
+
+        /// <summary>
+        /// The speed SwarmObjects should attempt to maintain.
+        /// </summary>
+        [SerializeField] public float TargetSpeed = 1;
+
+        /// <summary>
+        /// The rate at which the object rotates in degrees per second.
+        /// </summary>
+        [SerializeField] public float RotationSpeed = 15;
+
+
+        /// <summary>
+        /// A global multiplier to determine how random certain things are.
+        /// </summary>
+        [SerializeField] public float RandomFactor = 0.05f;
+
+        // Defines the size of the spawn area.
+        [SerializeField] float spawnBounds = 50;
+
+        /// <summary>
+        /// The current size of the swarm.
+        /// </summary>
+        public float Count { get { return Swarm.Count; } }
+
+        public static SwarmManager Instance;
+
+        /// <summary>
+        /// The list of all objects in the swarm.
+        /// </summary>
+        public List<SwarmObject> Swarm = new();
+
+        public void Start()
+        {
+            if (Instance == null) { Instance = this; }
+
+            SpawnSwarm();
+        }
+
+        /// <summary>
+        /// Get the average of the position of the swarm.
+        /// </summary>
+        public Vector3 GetAvgPosition()
+        {
+            // get the sum of all positions.
+            Vector3 sumVector = Vector3.zero;
+            foreach (SwarmObject swarmObject in Swarm)
+            {
+                sumVector += swarmObject.transform.position;
+            }
+
+            float x, y, z;
+            x = sumVector.x;
+            y = sumVector.y;
+            z = sumVector.z;
+
+            // divide everything by count, throw into a new vector.
+            Vector3 avg = new(x / Count, y / Count, z / Count);
+
+            return avg;
+        }
+
+        /// <summary>
+        /// Get average rotation vector of the swarm.
+        /// </summary>
+        /// <returns>The swarm's average rotation in Euler degrees.</returns>
+        public Vector3 GetAvgRotation()
+        {
+            // get the sum of all rotations
+            Vector3 sumVector = Vector3.zero;
+            foreach (SwarmObject swarmObject in Swarm)
+            {
+                sumVector += swarmObject.transform.rotation.eulerAngles;
+            }
+
+            float x, y, z;
+            x = sumVector.x;
+            y = sumVector.y;
+            z = sumVector.z;
+
+            // divide everything by count, throw into new vector.
+            Vector3 avg = new(x / Count, y / Count, z / Count);
+
+            return avg;
+        }
+
+        /// <summary>
+        /// This spawns the swarm.
+        /// </summary>
+        void SpawnSwarm()
+        {
+            for (int i = 0; i < spawnCount; i++)
+            {
+                // randomly generate position
+                float x, y, z;
+                x = Random.Range(-spawnBounds, spawnBounds);
+                y = Random.Range(-spawnBounds, spawnBounds);
+                z = Random.Range(-spawnBounds, spawnBounds);
+                //x = Random.Range(-1, 1);
+                //y = Random.Range(-1, 1);
+                //z = Random.Range(-1, 1);
+                Vector3 spawnPosition = new(x, y, z);
+
+                // create a new object and set its position.
+                SwarmObject obj = Instantiate(prefab).AddComponent<SwarmObject>();
+                obj.name = i.ToString();
+                obj.transform.position = spawnPosition;
+                Swarm.Add(obj);
+            }
+        }
+    }
+}
---
+++ Assets/Script/SwarmObject.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwarmTesting
+{
+    /// <summary>
+    /// An object to be controlled by the SwarmManager
+    /// </summary>
+    internal class SwarmObject : MonoBehaviour
+    {
+        // Apparently using unity, i dont need to assign the singleton thingy for the scriptable object. Idk?
+        SwarmManager swarmManager = SwarmManager.Instance;
+
+        // This will be a trigger to determine if something is too close.
+        SphereCollider sphereCollider;
+
+        List<GameObject> avoidanceList = new List<GameObject>();
+
+        public void Start()
+        {
+            sphereCollider = gameObject.AddComponent<SphereCollider>();
+            sphereCollider.radius = swarmManager.AvoidanceDist / 2;
+            sphereCollider.isTrigger = true;
+        }
+
+        public void Update()
+        {
+            // Documenting what i want to happen...
+            // Rotation should first, adjust to average.
+            // If far from center, rotate towards center.
+            // Then, rotate away from anything absurdly close.
+            // Give it some randomness.
+            // Lastly, move the object
+
+            float delta = Time.deltaTime;
+
+            // Set to avg position. If nothing else, we should go towards the average.
+            Vector3 avgRot = swarmManager.GetAvgRotation();
+            Vector3 targetRot = avgRot;
+
+            // Based on the distance, determine how much we should rotate towards the average.
+            Vector3 avgPos = swarmManager.GetAvgPosition();
+            targetRot = TargetAvg(targetRot, avgPos);
+            targetRot = AvoidAvoidances(targetRot);
+
+            // Add a random value based on random factor.
+            float x = Random.Range(-1, 1) * 180 * swarmManager.RandomFactor;
+            float y = Random.Range(-1, 1) * 180 * swarmManager.RandomFactor;
+            float z = Random.Range(-1, 1) * 180 * swarmManager.RandomFactor;
+            targetRot += new Vector3(x, y, z);
+
+            // Apply the rotation.
+            Quaternion newRot = new(); newRot.eulerAngles = targetRot;
+            gameObject.transform.rotation = newRot;
+
+            // Move the object in the given direction by target speed divided by delta.
+            float moveRandFactor = 1 + (Random.Range(-0.5f,0.5f) * swarmManager.RandomFactor);
+            float finalSpeed = swarmManager.TargetSpeed * delta * moveRandFactor;
+            transform.position += transform.forward * finalSpeed;
+        }
+
+        private Vector3 TargetAvg(Vector3 targetRot, Vector3 avgPos)
+        {
+            float dist = Vector3.Distance(transform.position, avgPos);
+
+            // Scale and clamp distance to be used as a lerp factor
+            float offset = swarmManager.MinRecallDistance;
+            float max = swarmManager.MaxRecallDistance - offset;
+            float recallLerpFactor = Mathf.Clamp01((dist - offset) / max);
+
+            Vector3 recallTarget = Quaternion.FromToRotation(transform.position, avgPos).eulerAngles;
+            Debug.Log($"Recall target is {recallTarget}");
+
+            // Update target.
+            targetRot = Vector3.Slerp(targetRot, recallTarget, recallLerpFactor);
+            return targetRot;
+        }
+
+        private Vector3 AvoidAvoidances(Vector3 targetRot)
+        {
+            // Deal with avoidances.
+            RemoveOldAvoidances();
+            GameObject closestObj = null;
+            float closestObjDist = 0;
+            foreach (GameObject obj in avoidanceList)
+            {
+                // Get distance between us and obj.
+                float distance = Vector3.Distance(transform.position, obj.transform.position);
+                // Track closest object.
+                if (distance > closestObjDist) { closestObjDist = distance; closestObj = obj; }
+            }
+
+            if (closestObj == null) { return transform.rotation.eulerAngles; }
+
+            // divide by the threshold to get a number from 0 to 1, then use as factor.
+            float avoidanceLerpFactor = closestObjDist / swarmManager.AvoidanceDist;
+
+            // Get a target and log it, then update.
+            Vector3 avoidanceTarget = Quaternion.Inverse(
+                Quaternion.FromToRotation(
+                    transform.position, closestObj.transform.position)
+                ).eulerAngles;
+            Debug.Log($"Avoidance target is {avoidanceTarget}");
+
+            // Update target.
+            targetRot = Vector3.Slerp(targetRot, avoidanceTarget, avoidanceLerpFactor);
+            return targetRot;
+        }
+
+        void RemoveOldAvoidances()
+        {
+            bool recurse = false;
+            do
+            {
+                foreach (GameObject obj in avoidanceList)
+                {
+                    if (Vector3.Distance(transform.position, obj.transform.position) > swarmManager.AvoidanceDist)
+                    {
+                        avoidanceList.Remove(obj);
+                        // Because c# can't continue iteration after modification, set the recurse flag so we can restart the loop.
+                        recurse = true; break;
+                    }
+                }
+            } while (recurse);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            avoidanceList.Add(other.gameObject);
+        }
+    }
+}
